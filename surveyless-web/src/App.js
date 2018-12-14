a430dc6f87bd214ab3a4deeac1db08d7f813e018
Lauren Maslanka <laurenmaslanka@laurens-macbook-pro.local>
2018-12-14 15:48:55 -0500

    added a UI for the survey creation

--- surveyless-web/src/App.js
+++ surveyless-web/src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import {Router} from 'react-dom'
+import {ControlLabel, FormControl, FormGroup, Button, Form, Col, ButtonGroup} from 'react-bootstrap'
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value:''
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+       value: e.target.value
+    })
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+              <h1 className=".bs-docs-header .container">
+                Welcome to the Ultimate Event Management Tool
+              </h1>
         </header>
+
+        <div className="jumbotron">
+
+            <h1>Create a New Survey!</h1>
+        </div>
+
+        <Form horizontal>
+           <FormGroup  controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} xs={2} sm={2} md={2}>
+                     Event Name
+                   </Col>
+                <Col xs={8} sm={10} md={8}>
+                   <FormControl
+                     type="text"
+                     value={this.state.response}
+                     placeholder="2018 Global Hackathon"
+                     onChange={this.handleChange}
+                   />
+               </Col>
+           </FormGroup>
+
+           <FormGroup  controlId="formHorizontalEmail">
+                           <Col componentClass={ControlLabel} xs={2} sm={2} md={2}>
+                                Presenter
+                              </Col>
+                           <Col xs={8} sm={10} md={8}>
+                              <FormControl
+                                type="text"
+                                value={this.state.response}
+                                placeholder="Maybe You"
+                                onChange={this.handleChange}
+                              />
+                          </Col>
+                      </FormGroup>
+
+            <FormGroup>
+                <Col sm={2}>
+                  <Button type="submit">Create Survey</Button>
+                </Col>
+              </FormGroup>
+        </Form>
+
       </div>
     );
   }
