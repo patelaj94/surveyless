9e2d913c379f0af498644f10f9d1ebed02c5fabf
ChrisCruze <cruzc09@gmail.com>
2019-06-27 16:28:27 -0400

    updated for modal using sweetalert

--- surveyless-web/src/components/Card.js
+++ surveyless-web/src/components/Card.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import 'components/Card.css'
+import React from "react";
+import "components/Card.css";
 
-export default function (props) {
-	return (
-		<div class="card">
-			<div class="card-content">
-				<div class="content">
-					<h1 class="title is-5">
-						{props.name}
-					</h1>
-					<h2 class="subtitle is-7">
-						{props.question}
-					</h2>
-				</div>
-			</div>
-			<footer class="card-footer">
-				<a href="#" class="card-footer-item"></a>
-				<a href="#" class="card-footer-item">View</a>
-				<a href="#" class="card-footer-item"></a>
-			</footer>
-		</div>
-	)
+export default function(props) {
+  return (
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          <h1 class="title is-5">{props.name}</h1>
+          <h2 class="subtitle is-7">{props.question}</h2>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">
+          Take Survey
+        </a>
+        <a href="#" class="card-footer-item">
+          View Results
+        </a>
+      </footer>
+    </div>
+  );
 }
