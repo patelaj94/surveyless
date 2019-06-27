c7be34098e26a1a1a6f20881fcbdc9c83995d357
woat <ahmad.s.rusdi@gmail.com>
2019-06-27 14:48:06 -0400

    api working with proxy

--- surveyless-web/src/App.js
+++ surveyless-web/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
+import Survey from 'api/survey/Survey'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+	async componentDidMount() {
+		console.log(Survey)
+		const res = await Survey.getAll()
+		console.log(await res)
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
