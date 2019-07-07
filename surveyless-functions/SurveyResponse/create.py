e3548076f33b1281765027a8c01b9fe54c621c42
arjunpatel <patelaj94@yahoo.com>
2019-07-07 16:28:46 -0400

    Changing create survey response

--- surveyless-functions/SurveyResponse/create.py
+++ surveyless-functions/SurveyResponse/create.py
@@ -7,8 +7,8 @@ import uuid
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
-
 def create(event, context):
+    
     data = json.loads(event['body'])['body']
     if 'survey_unique_id' not in data:
         logging.error("Validation Failed")
@@ -18,8 +18,16 @@ def create(event, context):
     timestamp = int(time.time() * 1000)
 
     table = dynamodb.Table("surveyResponses")
-
-    survey = {
+    
+    print(data['survey_unique_id'])
+    surveyId = data['survey_unique_id']
+    responseText = data['question_text']
+    
+    ## If table does not exist, put new item
+    item = table.get_item(Key = {'survey_unique_id':surveyId})
+    if(not "Item" in item):
+        print('twas null')
+        response = {
         'survey_unique_id': data['survey_unique_id'],
         'answer': data['question_text'],
         'totalYes': 0,
@@ -27,15 +35,40 @@ def create(event, context):
         'active' : True,
         'createdAt': timestamp,
         'updatedAt': timestamp
-    }
+        }
+        table.put_item(Item=response)
     
-    # write the todo to the database
-    table.put_item(Item=survey)
+    
+    
+    ## Increment Logic
+    myIncrement = 1
+    if(responseText=='yes'):
+        response = table.update_item(
+        Key = {
+            'survey_unique_id': surveyId
+        },
+        UpdateExpression="set totalYes = totalYes + :val",
+        ExpressionAttributeValues={
+        ':val': myIncrement
+        }
+    )
+    
+    if(responseText=='no'):
+        response = table.update_item(
+        Key = {
+            'survey_unique_id': surveyId
+        },
+        UpdateExpression="set totalNo = totalNo + :val",
+        ExpressionAttributeValues={
+        ':val': myIncrement
+        }
+    )
+    
 
     # create a response
     response = {
         "statusCode": 200,
-        "body": json.dumps(survey)
+        "body": json.dumps(response)
     }
 
     return response
